0b69b522c29328023fc62f5375bd01365ef0a2dc
abdAlmajeed <abd.almajed.alhashemi@gmail.com>
2026-05-03 17:23:54 +0300

    edit image site

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -2,26 +2,30 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import I18nProvider from "@/components/I18nProvider";
 export const metadata = {
-  title: "Bouw & Renovatie Diensten | Bedrijfsnaam",
-  description: "Professionele bouw- en renovatiediensten in Nederland. Kwaliteitswerk, betrouwbare uitvoering en scherpe prijzen. Vrijblijvende offerte!",
+  title: "Rivo",
+  description:
+    "Professionele bouw- en renovatiediensten in Nederland. Kwaliteitswerk, betrouwbare uitvoering en scherpe prijzen. Vrijblijvende offerte!",
+  keywords: [
+    "aannemer Nederland",
+    "renovatie",
+    "bouwbedrijf",
+    "badkamer renovatie",
+    "keuken installatie",
+  ],
+
   openGraph: {
-    title: "Bouw & Renovatie Diensten | Bedrijfsnaam",
-    description: "Uw betrouwbare partner voor al uw bouwwerkzaamheden. Van volledige huisrenovaties tot badkamerrenovaties, keukeninstallaties, dakreparaties en schilderwerk. Wij leveren hoogwaardige resultaten op tijd en binnen budget.",
-    keywords: [
-  "aannemer Nederland",
-  "renovatie",
-  "bouwbedrijf",
-  "badkamer renovatie",
-  "keuken installatie",
-],
-    url: "https://rivoaannemerbedrijf.nl/", 
-    siteName: "Bedrijfsnaam",
+    title: "Rivo Diensten",
+    description:
+      "Uw betrouwbare partner voor al uw bouwwerkzaamheden. Van volledige huisrenovaties tot badkamerrenovaties, keukeninstallaties, dakreparaties en schilderwerk. Wij leveren hoogwaardige resultaten op tijd en binnen budget.",
+
+    url: "https://rivoaannemerbedrijf.nl/",
+    siteName: "Rivo",
     images: [
       {
-        url: 'https://rivoaannemerbedrijf.nl/heroHome.jpg', 
+         url: "/heroHome.jpg",
         width: 1200,
         height: 630,
-        alt: "Professionele bouw- en renovatiediensten",
+        alt: "Rivo",
       },
     ],
     type: "website",
@@ -32,7 +36,6 @@ export default function SiteLayout({
 }: {
   children: React.ReactNode;
 }) {
-  
   return (
     <I18nProvider>
       <div className="flex flex-col min-h-screen">
